4e783d2c9da491c34ae3bd966305335501ef60c8
Lucas Alexandre <lucasalexandre.lads13@gmail.com>
2026-03-16 19:45:02 -0300

    feat: finaliza o exercício 03 - calculadora de média

--- 03-calculadora-media/app.js
+++ 03-calculadora-media/app.js
@@ -1,8 +1,8 @@
 'use strict'
 
 const validarIntervaloNumericoNota = function(nota1, nota2, nota3){
-    
-    if(nota1 >= 0 && nota1 <= 10 || nota2 >= 0 && nota2 <= 10 || nota3 >= 0 && nota3 <= 10){
+
+    if(nota1 < 0 || nota1 > 10 || nota2 < 0 || nota2 > 10 || nota3 < 0 || nota3 > 10){
         return false
     }
 
@@ -14,12 +14,42 @@ const calcularMedia = function(){
     const notaAluno1 = document.getElementById('nota1')
     const notaAluno2 = document.getElementById('nota2')
     const notaAluno3 = document.getElementById('nota3')
-    
-    let resultadoMedia = (notaAluno1.value + notaAluno2.value + notaAluno3.value) / 3
-    let statusAluno
+    const resultado  = document.getElementById('resultado')
 
-    if(resultadoMedia >= 7){
-        
+    let nota1 = Number(notaAluno1.value)
+    let nota2 = Number(notaAluno2.value)
+    let nota3 = Number(notaAluno3.value)
+    let numeroInvalido = 'Não foi possível realizar o cálculo de Média. Digite apenas números válidos (0 até 10)'
+    
+    if(!validarIntervaloNumericoNota(nota1, nota2, nota3)){
+        resultado.textContent = numeroInvalido
+        return
     }
 
+    let resultadoMedia = Number( (nota1 + nota2 + nota3) / 3 ).toFixed(2)
+    let statusAluno
+
+    removerClasses()
+
+    if(resultadoMedia >= 7){
+        statusAluno =  'Aprovado'
+        resultado.textContent = `A média do Aluno(a) foi de: ${resultadoMedia} Status do Aluno(a): ${statusAluno}`
+        resultado.classList.add('aprovado')
+
+    }else if(resultadoMedia >= 5 && resultadoMedia < 7){
+        statusAluno = 'Recuperação'
+        resultado.textContent = `A média do Aluno(a) foi de: ${resultadoMedia} Status do Aluno(a): ${statusAluno}`
+        resultado.classList.add('recuperacao')
+    
+    }else{
+        statusAluno = 'Reprovado'
+        resultado.textContent = `A média do Aluno(a) foi de: ${resultadoMedia} Status do Aluno(a): ${statusAluno}`
+        resultado.classList.add('reprovado')
+    }
 }   
+
+const removerClasses = function(){
+    const resultado = document.getElementById('resultado')
+    resultado.classList.remove('aprovado', 'recuperacao', 'reprovado')
+}
+
